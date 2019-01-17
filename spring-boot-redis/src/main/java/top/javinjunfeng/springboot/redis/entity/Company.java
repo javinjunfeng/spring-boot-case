f4c85e2c5a5ea7859cb28533f098cec847005d5e
zhangjunfeng <1070570821>
2019-01-17 17:58:37 +0800

    spring-boot-redis模块

---
+++ spring-boot-redis/src/main/java/top/javinjunfeng/springboot/redis/entity/Company.java
@@ -0,0 +1,35 @@
+package top.javinjunfeng.springboot.redis.entity;
+
+import java.io.Serializable;
+
+
+public class Company implements Serializable {
+
+    private String companyName;
+
+    private Integer label;
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public Integer getLabel() {
+        return label;
+    }
+
+    public void setLabel(Integer label) {
+        this.label = label;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "companyName='" + companyName + '\'' +
+                ", label=" + label +
+                '}';
+    }
+}
---
+++ spring-boot-redis/src/main/java/top/javinjunfeng/springboot/redis/util/KeyUtil.java
@@ -0,0 +1,8 @@
+package top.javinjunfeng.springboot.redis.util;
+
+
+public class KeyUtil {
+    public final static String COMPANY_KEY = "ResumeZero:company:";
+
+    public final static String SCHOOL_KEY = "ResumeZero:school:";
+}
