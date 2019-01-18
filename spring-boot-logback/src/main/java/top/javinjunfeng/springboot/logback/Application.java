cffef872e127dbc3fd028069ffaf7b911ee9c04b
zhangjunfeng <1070570821>
2019-01-18 17:46:44 +0800

    spring-boot-logback模块

--- spring-boot-logback/src/main/java/top/javinjunfeng/springboot/logback/Application.java
+++ spring-boot-logback/src/main/java/top/javinjunfeng/springboot/logback/Application.java
@@ -3,6 +3,9 @@ package top.javinjunfeng.springboot.logback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author zhangjunfeng
+ */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
