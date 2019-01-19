2a5ddcab2db52261e42b847cc49fe9fd6f9e046a
zhangjunfeng <1070570821>
2019-01-19 18:40:19 +0800

    spring-boot-thymeleaf模块

---
+++ spring-boot-thymeleaf/src/main/java/top/javinjunfeng/springboot/thymeleaf/ThymeleafApplication.java
@@ -0,0 +1,18 @@
+package top.javinjunfeng.springboot.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: javinjunfeng
+ * @Date: 2019/1/19 2:38 PM
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class ThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafApplication.class, args);
+    }
+
+}
